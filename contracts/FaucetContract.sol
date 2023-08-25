71d2c888803084e55ffd6e0b6e61e759afdfc2e0
CCnova <caiocnova1@gmail.com>
2023-08-25 11:54:01 -0300

    Implement more faucet contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,38 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint public funds = 1000;
-    int public counter = -10;
-    uint32 public test = 4294967295;
+    address[] public funders;
+
+    // this is a special function
+    // it's called when you make a tx that doesn't specify
+    // funciton name to call
+    // External function are part of the contract interface
+    // which means they can be called via contracts and other tx
+    receive() external payable {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+    }
+
+    // pure, view - read-only call, no gas fee
+    // view - It indicates that the function will not alter the storage state in any way
+    // pure - Even more strict, indicating that it won't even read the storage state
+
+    // Transaction can generate state changes and require gas fee
+
+    // To talk to the node on the network you can make JSON-RPC http calls
 }
+
+// Block info
+// Nonce - a hash that when combined with the minHash proofs that
+// the block has gone through proof of work (POW)
+// 8 bytes => 64 bits
